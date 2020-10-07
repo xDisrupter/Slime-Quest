e89e9214c126894ae363c7c27a1d6e5b0ed9d4e5
xDisrupter <montaguelafrance@yahoo.com>
2020-10-07 11:46:21 -0700

    Enemy Patrol Script & Turn Prefab

--- Slime Quest/Assets/Scripts/Patrol.cs
+++ Slime Quest/Assets/Scripts/Patrol.cs
@@ -6,7 +6,7 @@ public class Patrol : MonoBehaviour
 {
     public float speed;
     public float distance;
-    private bool movingRight = true;
+    public bool movingRight = true;
     public Transform groundDetection;
     public RaycastHit hit;
     // Start is called before the first frame update
@@ -16,19 +16,32 @@ public class Patrol : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+private void Update()
     {
-        Vector3 direction = new Vector3(1f* speed * Time.deltaTime,0f,0f);
-        transform.Translate(direction);
-        Vector3 down = transform.TransformDirection(Vector3.down);
-
-
-        bool groundInfo = Physics.Raycast(groundDetection.position, down, distance);
-
-        if(groundInfo ==true)
-        {
-            Debug.Log("on ground");
-        }
-        else Debug.Log("Not ground");
+        //transform.Translate(Vector2.right * speed * Time.deltaTime);
+    if(movingRight){
+    transform.Translate(2 * Time.deltaTime *speed, 0,0);
+    //transform.localScale = new Vector2(2,2);
     }
+    else{
+        transform.Translate(-2 * Time.deltaTime * speed, 0,0);
+        //transform.localScale = new Vector2(-2,2);
+    }
+    }
+
+private void OnTriggerEnter(Collider collider)
+{
+   // if(collider.gameObject.CompareTag("turn")){
+        if(movingRight ==true)
+        {
+            movingRight = false;
+           // Debug.Log("moveing Right");
+        }
+        else{
+            movingRight = true;
+           // Debug.Log("Moving Left");
+        }   
+    //}
+}
+
 }
